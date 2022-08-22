37f0ae87aab34370d0e95df619a76b7884bb6f0a
mercedes sandu <mxrcedes@gmail.com>
2022-08-22 09:36:33 -0500

    main.py setup

---
+++ settings.py
@@ -0,0 +1,19 @@
+from pygame.math import Vector2
+
+# Screen
+SCREEN_WIDTH = 1280
+SCREEN_HEIGHT = 740
+# TITLE_SIZE = 64
+
+# # Overlay positions
+# OVERLAY_POSITIONS = {
+#     'tool': (40, SCREEN_HEIGHT - 15),
+#     'seed': (70, SCREEN_HEIGHT - 5)
+# }
+
+# PLAYER_TOOL_OFFSET = {
+#     'left': Vector2(-50, 40),
+#     'right': Vector2(50, 40),
+#     'up': Vector2(0, -10),
+#     'down': Vector2(0, 50)
+# }
