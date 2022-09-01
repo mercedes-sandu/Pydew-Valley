a0a03da2860e06349f71d85ddf5177aea05e526e
mercedes sandu <mxrcedes@gmail.com>
2022-08-31 23:48:12 -0500

    tool animations/use

--- player.py
+++ player.py
@@ -1,6 +1,7 @@
 import pygame
 from settings import *
 from support import *
+from timer import Timer
 
 class Player(pygame.sprite.Sprite):
     def __init__(self, pos, group):
@@ -21,7 +22,20 @@ class Player(pygame.sprite.Sprite):
         self.pos = pygame.math.Vector2(self.rect.center)
         self.speed = 200
 
+        # Timers
+        self.timers = {
+            'tool use': Timer(350, self.use_tool)
+        }
+
+        # Tools
+        self.selected_tool = 'axe'
+
+    def use_tool(self):
+        """Allows the player to use its selected tool."""
+        print(self.selected_tool)
+
     def import_assets(self):
+        """Imports all of the assets for the player."""
         self.animations = {
             'up': [],
             'down': [],
@@ -62,36 +76,49 @@ class Player(pygame.sprite.Sprite):
         """Processes input for the player."""
         keys = pygame.key.get_pressed()
 
-        if keys[pygame.K_UP]:
-            self.direction.y = -1
-            self.status = 'up'
-        elif keys[pygame.K_DOWN]:
-            self.direction.y = 1
-            self.status = 'down'
-        else: 
-            self.direction.y = 0
-        
-        if keys[pygame.K_RIGHT]:
-            self.direction.x = 1
-            self.status = 'right'
-        elif keys[pygame.K_LEFT]:
-            self.direction.x = -1
-            self.status = 'left'
-        else:
-            self.direction.x = 0
+        if not self.timers['tool use'].active:
+            # Directions
+            if keys[pygame.K_UP]:
+                self.direction.y = -1
+                self.status = 'up'
+            elif keys[pygame.K_DOWN]:
+                self.direction.y = 1
+                self.status = 'down'
+            else: 
+                self.direction.y = 0
+            
+            if keys[pygame.K_RIGHT]:
+                self.direction.x = 1
+                self.status = 'right'
+            elif keys[pygame.K_LEFT]:
+                self.direction.x = -1
+                self.status = 'left'
+            else:
+                self.direction.x = 0
+
+            # Tool use
+            if keys[pygame.K_SPACE]:
+                self.timers['tool use'].activate()
+                self.direction = pygame.math.Vector2()
+                self.frame_index = 0
 
     def get_status(self):
         """Sets status of the player appropriately."""
-
         # Idle
         if self.direction.magnitude() == 0:
             self.status = self.status.split('_')[0] + '_idle'
 
         # Tool use
+        if self.timers['tool use'].active:
+            self.status = self.status.split('_')[0] + '_' +  self.selected_tool
+
+    def update_timers(self):
+        """Updates the player's timers."""
+        for timer in self.timers.values():
+            timer.update()
 
     def move(self, dt):
         """Moves the player's pos."""
-
         # Normalize vectors for diagonal speed
         if self.direction.magnitude() > 0:
             self.direction = self.direction.normalize()
@@ -108,5 +135,6 @@ class Player(pygame.sprite.Sprite):
         """Updates the player."""
         self.input()
         self.get_status()
+        self.update_timers()
         self.move(dt)
         self.animate(dt)
---
+++ timer.py
@@ -0,0 +1,29 @@
+from asyncio import current_task
+import pygame
+
+class Timer:
+    def __init__(self, duration, func = None):
+        """Initializes a timer with the specified duration and function."""
+        self.duration = duration
+        self.func = func
+        self.start_time = 0
+        self.active = False
+
+    def activate(self):
+        """Activates/starts the timer."""
+        self.active = True
+        self.start_time = pygame.time.get_ticks()
+
+    def deactivate(self):
+        """Deactivates/stops the timer."""
+        self.active = False
+        self.start_time = 0
+
+    def update(self):
+        """Updates the timer, called continuously."""
+        current_time = pygame.time.get_ticks()
+        if current_time - self.start_time >= self.duration:
+            self.deactivate()
+            
+            if self.func:
+                self.func()
